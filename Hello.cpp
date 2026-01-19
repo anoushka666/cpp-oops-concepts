71ba1e3c4e61e74ecb2796ef541b2a30d9b83adf
anoushka666 <anoushka6807@gmail.com>
2026-01-19 17:53:30 +0530

    Add files via upload

--- Hello.cpp
+++ Hello.cpp
@@ -1,11 +1,47 @@
 #include<iostream>
+#include<string>
 using namespace std;
-class Point{
-    Point(){
-        cout<<"Constructor called";
+class Bank{
+    private:
+    string name;
+    int ac_no;
+    char type;   
+    double balance;
+
+    public:
+    void details(string n, int a, char t, double b){
+        name = n;
+        ac_no = a;
+        type = t;
+        balance = b;
+    }
+    
+    void deposit(int amount){
+    
+        balance += amount;
+    }
+
+    void withdraw(int amount){
+    
+        if ((balance-amount)>1000){
+            balance-= amount;
+        }
+        else{
+            cout<<"Cannot withdraw"<<endl;
+        }
+    }
+
+    void display(){
+        cout<<"Name of account holder = "<<name<<endl;
+        cout<<"Account number = "<<ac_no<<endl;
+        cout<<"Account type = "<<type<<endl;
+        cout<<"Balances = "<<balance;
     }
 };
 int main(){
-    Point p1;
-    return 0;
+    Bank b[10];
+    b[0].details ("mark", 9191, 'S', 5000);
+    b[0].deposit(500);
+    b[0].withdraw(500);
+    b[0].display();
 }
