71a3b5cb57c8faeb2a605d9c72c969323914f76e
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-01-22 21:12:33 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,58 +1,31 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class Student{
+
+class Wall{
     private:
-    int enroll_no;
-    string name;
-    string branch;
-    float cgpa;
+    int length;
+    int height;
 
     public:
-    Student(int en, string n, string b, float c){
-        enroll_no = en;
-        name = n;
-        branch = b;
-        cgpa = c;
+    Wall(int l, int h){
+        length = l;
+        height = h;
+    }
+    Wall(const Wall &w){
+        length = w.length;
+        height = w.height;
     }
 
-    Student(int en, string n, float c){
-        enroll_no = en;
-        name = n;
-        branch = "CSE";
-        cgpa = c;
-    }
-
-    Student(){
-        enroll_no = 1;
-        name = "Unknown";
-        branch = "CSE";
-        cgpa = 6;
-    }
-
-    void setcgpa(float a){
-        cgpa = a;
-    }
-
-    float getcgpa(){
-        return cgpa;
-    }
-
-    void print(){
-        cout<<endl<<"Name: "<<name<<endl;
-        cout<<"Enrollment number: "<<enroll_no<<endl;
-        cout<<"Branch: "<<branch<<endl;
-        cout<<"CGPA: "<<cgpa<<endl;
+    int area(){
+        return length*height;
     }
 };
 int main(){
-    Student s1 (123, "Anoushka", "CSE", 9.8);
-    Student s2 (124, "Mark", 8.7);
-    Student s3;
+    Wall w1(5,6);
+    cout<<"Area is = "<<w1.area()<<endl;
+    Wall w2(w1);
+    cout<<"Area (copied) is = "<<w2.area()<<endl;
 
-    s2.setcgpa(9);
-    s2.getcgpa();
-    s1.print();
-    s2.print();
-    s3.print();
+
 }
