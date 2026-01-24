782a1ce491e4aa1ee3a93223e542163deb58f55c
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-01-24 23:17:44 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -8,23 +8,21 @@ class BankAccount{
     double balance;
 
     public:
-//parametrized constructor
     BankAccount(const char *a, double b){
         balance = b;
         accountHolder = new char[strlen(a) +1];
         strcpy(accountHolder, a);
     }
-//copy constructor
+
     BankAccount(const BankAccount &b1){
         balance = b1.balance;
         accountHolder = new char(strlen(b1.accountHolder)+1);
         strcpy(accountHolder, b1.accountHolder);
     }
-//destructor
+
     ~BankAccount(){
         delete[] accountHolder;
     }
-//member function to print details
     void print(){
         cout<<"Name: "<<accountHolder<<endl;
         cout<<"Balance: "<<balance<<endl;
@@ -37,3 +35,4 @@ int main(){
     BankAccount b2 = b1;
     b2.print();
 }
+
