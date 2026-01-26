5263ed9a3ac44aab5320776230cfdbbad7f9ab8d
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-01-26 20:27:47 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,27 +1,22 @@
 #include<iostream>
 using namespace std;
-class Demo{
-    private:
-    //static data members 
-    static int X;
-    static int Y;
-
-    public:
-    //static member function 
-    static void print(){
-        cout<<"Value of X: "<<X<<endl;
-        cout<<"Value of Y: "<<Y<<endl;
-
-    }
-};
-//static data members initialization 
-int Demo::X = 10;
-int Demo::Y = 20;
 int main(){
-    Demo OB;
-    cout<<"Printing through object name"<<endl;
-    OB.print();
+    int A[3] = {10, 100, 200};
+    int *ptr = A;
 
-    cout<<"Accessing class with class name"<<endl;
-    Demo::print(); //preferred and correct way 
+    cout<<"After increment operator: "<<endl;
+    for (int i=0; i<3; i++){
+        cout<<"Address of var["<<i<<"] "<<ptr<<endl;
+        cout<<"Value of var["<<i<<"]" <<*ptr<<endl;
+        ptr++;
+    }
+
+    cout<<"After decrement operator: "<<endl;
+    for (int i=2; i>=0; i--){
+        cout<<"Address of var["<<i<<"] "<<ptr<<endl;
+        cout<<"Value of var["<<i<<"]" <<*ptr<<endl;
+        ptr--;
+    }
+
+
 }
