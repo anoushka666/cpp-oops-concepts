3d344da7e14a17893c198534ad054e8bccc47157
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-01-26 20:45:59 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,17 +1,38 @@
 #include<iostream>
+#include<string>
 using namespace std;
-int main(){
-    int n;
-    cout<<"Enter no. of elements: ";
-    cin>>n;
+class Bank{
+    private: 
+    string name;
+    int acc_no;
+    double balance;
 
-    int arr[n];
-    cout<<"Enter elements: "<<endl;
-    int sum=0;
-    for (int i=0; i<n; i++){
-        cin>>arr[i];
-        sum += arr[i];
+    public:
+    void assign(string n, int a, double b){
+        name = n;
+        acc_no = a;
+        balance = b;
     }
-    cout<<"Sum of elements: "<<sum;
-
+    void deposit(double amount){
+        balance+= amount;
+    }
+    void withdraw(double amount){
+        if(balance<amount){
+            cout<<"Not sufficient money"<<endl;
+        }
+        else {
+            balance-= amount;
+        }
+    }
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Balance: "<<balance<<endl;
+    }
+};
+int main(){
+    Bank b1;
+    b1.assign("Anoushka", 239, 1000);
+    b1.deposit(500);
+    b1.withdraw(5000);
+    b1.display();
 }
