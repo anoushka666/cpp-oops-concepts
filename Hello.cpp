682bbc617197b232381f3eb0302eb2efe733388d
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-01-28 21:23:34 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,52 +1,75 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
 using namespace std;
-class Vector{
-    private:
-    int size;
-    float v[100];
 
-    public:
-//to assign vector values
-    void assign(int n){
-        size = n;
-        cout<<"Enter "<<size<<" elements: "<<endl;
-        for (int i=0; i<size; i++){
-            cin>>v[i];
+class StudentRecord {
+private:
+    int rollNo;
+    int numSubjects;
+    float* marks;
+    char* name;
+
+public:
+    StudentRecord(int r, const char* n, int s) {
+        rollNo = r;
+        numSubjects = s;
+
+        name = new char[strlen(n) + 1];
+        strcpy(name, n);
+
+        marks = new float[numSubjects];
+        for (int i = 0; i < numSubjects; i++)
+            marks[i] = 0;
+    }
+
+    ~StudentRecord() {
+        delete[] name;
+        delete[] marks;
+        cout << "Memory freed!" << endl;
+    }
+
+    void readMarks() {
+        for (int i = 0; i < numSubjects; i++) {
+            cout << "Enter marks for subject " << i + 1 << ": ";
+            cin >> marks[i];
         }
     }
 
-//to add a scalar
-    void add(int s){
-        for (int i=0; i<size; i++){
-            v[i]+=s;
+    void showDetails() const {
+        float total = 0;
+
+        cout << "Name: " << name << endl;
+        cout << "Roll No: " << rollNo << endl;
+
+        for (int i = 0; i < numSubjects; i++) {
+            cout << "Marks of subject " << i + 1 << ": " << marks[i] << endl;
+            total += marks[i];
         }
+
+        cout << "Total: " << total << endl;
+        cout << "Percentage: " << (total / numSubjects) << "%" << endl;
     }
-//to multiply by a scalar
-    void multiply(int s){
-        for (int i=0; i<size; i++){
-            v[i]*=s;
+
+    float getHighestMark() const {
+        if (numSubjects <= 0)
+            return -1;
+
+        float max = marks[0];
+        for (int i = 1; i < numSubjects; i++) {
+            if (marks[i] > max)
+                max = marks[i];
         }
+        return max;
     }
-//to display the vectors
-    void display(){
-        for (int i=0; i<size; i++){
-            cout<<v[i]<<" ";
-        }
-    }
-  
 };
-int main(){
-    Vector v1;
-    v1.assign(3);
-    cout<<endl<<"Vector elements: "<<endl;
-    v1.display();
-    v1.add(10);
-    cout<<endl<<"Updated Vector elements: "<<endl;
-    v1.display();
-    v1.multiply(5);
-    cout<<endl<<"Updated Vector elements: "<<endl;
-    v1.display();
 
+int main() {
+    StudentRecord* s1 = new StudentRecord(239, "Anoushka", 3);
+
+    s1->readMarks();
+    cout << "Highest Marks: " << s1->getHighestMark() << endl;
+    s1->showDetails();
+
+    delete s1;
+    return 0;
 }
-
-
