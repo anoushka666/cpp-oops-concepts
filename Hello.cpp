4110b0f70bc6e82c79e4e8f0ba72375ed9eed78a
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-02-05 22:15:21 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,40 +1,33 @@
 #include <iostream>
 using namespace std;
 
-/* =======================
-   Employee Class (Q4a)
-   ======================= */
 class Employee {
     int empID;
     float basicSalary;
 
 public:
-    // Default constructor
+    
     Employee() {
         empID = 0;
         basicSalary = 0;
     }
 
-    // Parameterized constructor
     Employee(int id, float sal) {
         empID = id;
         basicSalary = sal;
     }
 
-    // Copy constructor
     Employee(Employee &e) {
         empID = e.empID;
         basicSalary = e.basicSalary;
     }
 
-    // Friend declarations
+   
     friend void calculateNetSalary(Employee);
     friend void transferEmployee(class Department &, class Department &);
 };
 
-/* =======================
-   Payroll Logic (Q4b)
-   ======================= */
+
 void calculateNetSalary(Employee e) {
     float allowance = e.basicSalary * 0.20; // 20%
     float deduction = e.basicSalary * 0.10; // 10%
@@ -47,9 +40,6 @@ void calculateNetSalary(Employee e) {
     cout << "Net Salary: " << netSalary << endl;
 }
 
-/* =======================
-   Department Class (Q4c)
-   ======================= */
 class Department {
     string deptName;
     int totalEmployees;
@@ -68,7 +58,6 @@ public:
     }
 };
 
-/* Friend function for transfer */
 void transferEmployee(Department &from, Department &to) {
     if (from.totalEmployees > 0) {
         from.totalEmployees--;
@@ -76,19 +65,17 @@ void transferEmployee(Department &from, Department &to) {
     }
 }
 
-/* =======================
-   Main Function
-   ======================= */
+
 int main() {
-    // Q4a: Constructor overloading
+    
     Employee e1;
     Employee e2(101, 30000);
     Employee e3(e2);
 
-    // Q4b: Net salary calculation
+
     calculateNetSalary(e2);
 
-    // Q4c: Department transfer
+
     Department d1("IT", 5);
     Department d2("HR", 3);
 
@@ -104,3 +91,4 @@ int main() {
 
     return 0;
 }
+
