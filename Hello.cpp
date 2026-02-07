974587be24181bf1dc2e0d7fd5fdb0157e644ddd
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-02-07 22:43:07 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,147 +1,107 @@
-
 #include <iostream>
 using namespace std;
 
-class Point {
-    int x, y;
+class Matrix {
+private:
+    int rows;
+    int cols;
+    int **data;
 
 public:
+    Matrix(int r, int c) {
+        rows = r;
+        cols = c;
+
+        data = new int*[rows];
+        for (int i = 0; i < rows; i++) {
+            data[i] = new int[cols];
+            for (int j = 0; j < cols; j++) {
+                data[i][j] = 0;
+            }
+        }
+    }
+
+    ~Matrix() {
+        for (int i = 0; i < rows; i++) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+
+    int getRows() {
+        return rows;
+    }
+
+ 
+    int getCols() {
+        return cols;
+    }
+
    
-    Point() {
-        x = 0;
-        y = 0;
+    void setElement(int i, int j, int value) {
+        data[i][j] = value;
+    }
+
+
+    Matrix add(Matrix &m) {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.data[i][j] = data[i][j] + m.data[i][j];
+            }
+        }
+        return result;
     }
 
     
-    Point(int a, int b) {
-        x = a;
-        y = b;
+    Matrix multiply(Matrix &m) {
+        Matrix result(rows, m.cols);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < m.cols; j++) {
+                result.data[i][j] = 0;
+                for (int k = 0; k < cols; k++) {
+                    result.data[i][j] += data[i][k] * m.data[k][j];
+                }
+            }
+        }
+        return result;
     }
 
-    
-    Point(Point &p) {
-        x = p.x;
-        y = p.y;
-    }
-
+   
     void display() {
-        cout << "Point (" << x << ", " << y << ")" << endl;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                cout << data[i][j] << " ";
+            }
+            cout << endl;
+        }
     }
 };
-
-
-class Distance {
-    int feet;
-    int inches;
-
-public:
-    Distance(int f = 0, int i = 0) {
-        feet = f;
-        inches = i;
-    }
-
-    friend void compareDistance(Distance, Distance);
-};
-
-void compareDistance(Distance d1, Distance d2) {
-    int total1 = d1.feet * 12 + d1.inches;
-    int total2 = d2.feet * 12 + d2.inches;
-
-    if (total1 > total2)
-        cout << "First distance is larger" << endl;
-    else
-        cout << "Second distance is larger" << endl;
-}
-
-class Account {
-    int accountNumber;
-    float balance;
-
-public:
-    Account(int acc, float bal) {
-        accountNumber = acc;
-        balance = bal;
-    }
-
-    friend void transferFunds(Account &, Account &, float);
-
-    void display() {
-        cout << "Account " << accountNumber
-             << " Balance: " << balance << endl;
-    }
-};
-
-void transferFunds(Account &from, Account &to, float amount) {
-    if (from.balance >= amount) {
-        from.balance -= amount;
-        to.balance += amount;
-    } else {
-        cout << "Insufficient balance!" << endl;
-    }
-}
-
-
-class Time {
-    int hours, minutes, seconds;
-
-public:
-    Time(int h = 0, int m = 0, int s = 0) {
-        hours = h;
-        minutes = m;
-        seconds = s;
-    }
-
-    friend Time addTime(Time, Time);
-
-    void display() {
-        cout << hours << ":" << minutes << ":" << seconds << endl;
-    }
-};
-
-Time addTime(Time t1, Time t2) {
-    Time result;
-
-    result.seconds = t1.seconds + t2.seconds;
-    result.minutes = t1.minutes + t2.minutes + (result.seconds / 60);
-    result.seconds %= 60;
-    result.hours = t1.hours + t2.hours + (result.minutes / 60);
-    result.minutes %= 60;
-
-    return result;
-}
-
-
 int main() {
+    Matrix A(2, 2);
+    Matrix B(2, 2);
 
-   
-    Point p1;
-    Point p2(4, 5);
-    Point p3(p2);
+    A.setElement(0, 0, 1);
+    A.setElement(0, 1, 2);
+    A.setElement(1, 0, 3);
+    A.setElement(1, 1, 4);
 
-    p1.display();
-    p2.display();
-    p3.display();
+    B.setElement(0, 0, 5);
+    B.setElement(0, 1, 6);
+    B.setElement(1, 0, 7);
+    B.setElement(1, 1, 8);
 
-    // (b) Distance
-    Distance d1(5, 6);
-    Distance d2(6, 2);
-    compareDistance(d1, d2);
+    Matrix C = A.add(B);
+    Matrix D = A.multiply(B);
 
-    
-    Account a1(101, 5000);
-    Account a2(102, 3000);
+    cout << "Addition:\n";
+    C.display();
 
-    transferFunds(a1, a2, 1000);
-    a1.display();
-    a2.display();
+    cout << "Multiplication:\n";
+    D.display();
 
-  
-    Time t1(2, 45, 50);
-    Time t2(1, 20, 30);
 
-    Time t3 = addTime(t1, t2);
-    t3.display();
-
-    return 0;
 }
 
