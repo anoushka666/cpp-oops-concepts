8a9832690c6ebaa03271adf22ea15b7417be72bc
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-02-13 18:15:18 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,87 +1,56 @@
 #include<iostream>
-#include<string>
+#include<string.h>
 using namespace std;
-class Account{            //base class
-    protected:   //private but accessible to child class
-    int account_num;
-    string holder_name;
-    double balance;
-    
-
+class TaxDeductibles;
+class Employee{
     public:
-    //initialize details
-    Account(int a, string b, double c):account_num(a), holder_name(b), balance(c){}
-    void deposit(double amount){
-        balance = balance + amount;
-        cout<<"New balance: "<<balance<<endl;
-    }
-    void withdraw(double amount){
-        if (amount>balance){
-            cout<<"Insufficient funds"<<endl;
-        }
-        else {
-            balance = balance - amount;
-            cout<<"New balance: "<<balance<<endl;
-        }
-    }
-    void display() const{
-        cout<<"Accout number: "<<account_num<<endl;
-        cout<<"Holder name: "<<holder_name<<endl;
-        cout<<"Balance: "<<balance<<endl;
+    int eid;
+    string eName;
+    double annualSalary;
 
-    }
+    friend void Tax(Employee, TaxDeductibles);
+
 };
-
-class SavingsAccount : public Account{
-    private:
-    double interest_rate;
-
+class TaxDeductibles{
     public:
-    SavingsAccount(int a, string b, double c, double d):Account(a,b,c), interest_rate(d){};
-    void apply_interest(){
-        double interest = balance * (interest_rate/100);
-        balance = balance + interest;
-        cout<<"New balance: "<<balance<<endl;
-    }
-    void display() const{
-        cout<<"Interest rate: "<<interest_rate<<"%"<<endl;
-    }
+    int eid;
+    double insurancePremium;
+    double homeLoanInterest;
+
+
+    friend void Tax(Employee, TaxDeductibles);
 };
+void Tax(Employee e1, TaxDeductibles t1){
+    double deduction;
+    double annualTax;
 
-class CurrentAccount : public Account{
-    private:
-    double overdraft_limit;
-
-    public:
-    CurrentAccount(int a, string b, double c, double d): Account(a,b,c), overdraft_limit(d){};
-    void withdraw(double amount){
-        if (amount > balance + overdraft_limit){
-            cout<<"Withdrawal exceeds limit"<<endl;
-        }
-        else{
-            balance = balance - amount;
-            cout<<"New balance: "<<balance<<endl;
-        }
+    if(e1.eid!= t1.eid){
+        cout<<"Sorry! Unable to compute tax as the eid dont match"<<endl;
     }
-    void display() const{
-        cout<<"Overdraft limit: "<<overdraft_limit<<endl;
+    else{
+        if(t1.insurancePremium>50000){
+            t1.insurancePremium = 50000;
+        }
+        deduction =  t1.insurancePremium + t1.homeLoanInterest;
+        cout<<"Your total deduction= "<<t1.insurancePremium<<" + "<<t1.homeLoanInterest<<" = "<<deduction<<endl;
+        
+
+        annualTax = 0.1* (e1.annualSalary - deduction);
+        cout<<"Your Annual Tax = 10% of ("<<e1.annualSalary <<" - "<<deduction<<") = "<<annualTax<<endl;
+    }
 
     }
-};
+
 int main(){
-    SavingsAccount s1(1001, "anoushka", 5000, 5);
-    s1.Account::display();
-    s1.display();
-    
-    s1.withdraw(20000);
-    s1.apply_interest();
+    Employee e1;
+    cout<<"Enter employee details: "<<endl;
+    cin>>e1.eid>>e1.eName>>e1.annualSalary;
 
-    cout<<endl;
+    TaxDeductibles t1;
+    cout<<"Enter Tax Deductibles: "<<endl;
+    cin>>t1.eid>>t1.insurancePremium>>t1.homeLoanInterest;
 
-    CurrentAccount c1(2001, "aana", 10000, 2000);
-    c1.Account::display();
-    c1.display();
+    Tax(e1,t1);
 
-    c1.withdraw(2000);
-    return 0;
+
 }
