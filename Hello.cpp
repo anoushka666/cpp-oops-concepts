6a6ae932db19230322e9f2ad15b016f7bf955267
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-02-14 16:33:08 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,62 +1,38 @@
 #include<iostream>
 using namespace std;
-class Forex;
-class Equity{
-    protected:
-    int buyPrice;
-    int CMP;
-    int quantity;
+class Time{
+    private:
+    int hours;
+    int mins;
 
     public:
-    Equity(){
-        cout<<"Enter details: ";
-        cin>>buyPrice>>CMP>>quantity;
-    }
-    void displayE(){
-        cout<<"Equity: "<<endl;
-        cout<<"Buying price: "<<buyPrice<<endl;
-        cout<<"CMP: "<<CMP<<endl;
-        cout<<"Quantity: "<<quantity<<endl;
-    }
-    friend void Compare(Equity, Forex);
-};
-class Forex{
-    protected:
-    int quotePrice;
-    int basePrice;
-    int lotSize;
-    int noofLots;
-
-    public:
-    Forex(){
-        cout<<"Enter details: ";
-        cin>>quotePrice>>basePrice>>lotSize>>noofLots;
-    }
-    void displayF(){
-        cout<<"Forex: "<<endl;
-        cout<<"Quote Price: "<<quotePrice<<endl;
-        cout<<"Base Price: "<<basePrice<<endl;
-        cout<<"Lot Size: "<<lotSize<<endl;
-        cout<<"Noof Lots: "<<noofLots<<endl;
+    void getTime(){
+        cout<<"Enter time in hours: ";
+        cin>>hours;
+        cout<<"Enter time in mins: ";
+        cin>>mins; 
     }
 
-    friend void Compare(Equity, Forex);
+    Time operator + (Time t){
+        Time temp;
+        temp.hours = hours + t.hours;
+        temp.mins = mins + t.mins;
+        
+        temp.hours = temp.hours + temp.mins/60;
+        temp.mins = temp.mins % 60;
+        return temp;
+    }
+
+    void display(){
+        cout<<"Total time: "<<hours<<" hours and "<<mins<<" mins"<<endl;
+    }
 };
-void Compare(Equity e1, Forex f1){
-    int yieldEquity = ((e1.buyPrice- e1.CMP)*e1.quantity)/e1.buyPrice;
-    int yieldForex = ((f1.quotePrice - f1.basePrice)*f1.lotSize*f1.noofLots)/(f1.quotePrice+ f1.basePrice);
-    if(yieldEquity> yieldForex){
-        cout<<"Equity is the better financial instrument"<<endl;
-        e1.displayE();
-    }
-    else{
-        cout<<"Forex is the better financial instrument"<<endl;
-        f1.displayF();
-    }
-     
-}
 int main(){
-    Equity e1;
-    Forex f1;
-    Compare(e1,f1);
+    Time t1;
+    t1.getTime();
+    Time t2;
+    t2.getTime();
+    Time t3 = t1+t2;
+    t3.display();
+
 }
