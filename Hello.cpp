aa96201d10b68aae50917caaf4f2e5d89a156d22
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-02-15 11:24:23 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,26 +1,38 @@
-#include<iostream>
-#include<cstdlib>  //for rand()
-#include<ctime>    //for srand()
+#include <iostream>
+#include <string>
 using namespace std;
 
-int flip(){
-    return rand()%2;  //returns 0 or 1
-}
-int main(){
-    int heads = 0;
-    int tails = 0;
-    srand(time(0));
-    for (int i=0; i<10 ; i++){
-        int result = flip();
-        if(result==1){
-            cout<<"Heads"<<endl;
-            heads++;
-        }
-        else{
-            cout<<"Tails"<<endl;
-            tails++;
-        }
+class Book {
+private:
+    string title;
+    string author;
+    int publicationYear;
+
+public:
+    Book(string t, string a, int y) {
+        title = t;
+        author = a;
+        publicationYear = y;
     }
-    cout<<"Total heads = "<<heads<<endl;
-    cout<<"Total tails = "<<tails<<endl;
+    friend ostream& operator<<(ostream& out, const Book& b);
+};
+
+ostream& operator<<(ostream& out, const Book& b) {
+    out << "Title: " << b.title << endl;
+    out << "Author: " << b.author << endl;
+    out << "Publication Year: " << b.publicationYear << endl;
+    return out;
+}
+
+int main() {
+    Book b1("The Alchemist", "Paulo Coelho", 1988);
+    Book b2("Atomic Habits", "James Clear", 2018);
+
+    cout << "Book 1 Details:\n";
+    cout << b1 << endl;
+
+    cout << "Book 2 Details:\n";
+    cout << b2 << endl;
+
+    return 0;
 }
