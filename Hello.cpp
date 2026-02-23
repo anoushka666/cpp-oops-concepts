9ff2bb6e71502b94d76df04fd5e53fbfa5ac9abb
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-02-23 22:42:22 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,30 +1,74 @@
 #include<iostream>
+#include<string>
 using namespace std;
-class Vehicle{
+class EWallet{
+    private:
+    int userId;
+    string userName;
+    double currentBalance;
+
     public:
-    virtual void display(){
-        cout<<"This is a vehicle"<<endl;
+    EWallet(){
+        cout<<"Enter user ID: ";
+        cin>>userId;
+        cout<<"Enter user name: ";
+        cin>>userName;
+        cout<<"Enter current balance: ";
+        cin>>currentBalance;
+        if(currentBalance<0){
+            currentBalance = 0;
+        }
     }
-    virtual ~Vehicle() {}  //virtaul destructor
+
+    void addMoney(double amount){
+        currentBalance = currentBalance + amount;
+        if(currentBalance<0){
+            currentBalance = 0;
+        }
+    }
+
+    void display(){
+        cout<<"User ID: "<<userId<<endl;
+        cout<<"User name: "<<userName<<endl;
+        cout<<"Balance: "<<currentBalance<<endl;
+    }
+
+    void transfer(EWallet e1, double amount){
+        currentBalance = currentBalance - amount;
+        e1.currentBalance = e1.currentBalance + amount;
+        if(currentBalance<0){
+            currentBalance = 0;
+        }
+    }
+    void operator-(double amount) {
+        if (amount <= currentBalance) {
+            currentBalance -= amount;
+        } else {
+            cout << "Insufficient balance!\n";
+        }
+    }
+
+
+    friend ostream& operator <<(ostream& out, const EWallet& e1);
 };
-class Car: public Vehicle{
-    public:
-    void display() override{   //override
-        cout<<"This is a car"<<endl;
-    }};
-class Bike: public Vehicle{
-    public:
-    void display() override{
-        cout<<"This is a bike"<<endl;
-    }};
+
+    ostream& operator <<(ostream& out, const EWallet& e1){
+        out<<"User name: "<<e1.userName<<endl;
+        out<<"User ID: "<<e1.userId<<endl;
+        out<<"Balance: "<<e1.currentBalance<<endl;
+        return out;
+
+
+    }
 int main(){
-    Vehicle *vehicle[2];
-    vehicle[0] = new Car();
-    vehicle[1] = new Bike();
-    for (int i=0; i<2; i++){
-        vehicle[i]->display();
-    }
-    for (int i=0; i<2; i++){
-        delete vehicle[i];
-    }
+    EWallet e1;
+    EWallet e2;
+
+    e1.addMoney(4000);
+    e1 - 1000;
+    cout<<e1;
+    
+    e1.transfer(e2, 300);
+    cout<<e2;
+    cout<<e1;
 }
