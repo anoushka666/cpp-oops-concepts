17c090e79ce0323e566fc2d245ba5d00ed626f66
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-03-20 23:33:17 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,35 +1,70 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
 using namespace std;
-class Box{
-    private:
-    int length;
-    int breadth;
-    int height;
 
-    public:
-    Box(int l, int b, int h){
+class Container {
+private:
+    int length, width, height;
+
+public:
+    Container(int l = 0, int w = 0, int h = 0) {
         length = l;
-        breadth = b;
+        width = w;
         height = h;
     }
-    void display(){
-        cout<<"Length: "<<length<<endl;
-        cout<<"Breadth: "<<breadth<<endl;
-        cout<<"Height: "<<height<<endl;
+
+    int getLength() const { return length; }
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+
+    int volume() const {
+        return length * width * height;
     }
 
-    friend void calculateVol(Box b);
-
+    // Declare non-member functions as friends
+    friend Container operator*(const Container&, int);
+    friend ostream& operator<<(ostream&, const Container&);
 };
-void calculateVol(Box b){
-    cout<<"Volume: "<<b.breadth*b.height*b.length<<endl;
 
+// Non-member operator * (scaling)
+Container operator*(const Container& c, int k) {
+    int l = c.length * k;
+    int w = c.width * k;
+    int h = c.height * k;
+
+    if (l > 1000) l = 1000;
+    if (w > 1000) w = 1000;
+    if (h > 1000) h = 1000;
+
+    return Container(l, w, h);
 }
-int main(){
-    Box b1(10,5,10);
-    b1.display();
-    calculateVol(b1);
-return 0;
-    //this is a weak assigner's question
+
+// Non-member operator <<
+ostream& operator<<(ostream& out, const Container& c) {
+    out << "Length = " << c.length
+        << ", Width = " << c.width
+        << ", Height = " << c.height
+        << ", Volume = " << c.volume();
+    return out;
+}
+
+int main() {
+    int l, w, h, k;
+
+    cout << "Enter container dimensions (L W H): ";
+    cin >> l >> w >> h;
+
+    cout << "Enter scaling factor: ";
+    cin >> k;
+
+    Container c1(l, w, h);
+
+    cout << "\nOriginal Container:\n";
+    cout << c1 << endl;
+
+    Container c2 = c1 * k;
+
+    cout << "\nScaled Container:\n";
+    cout << c2 << endl;
+
+    return 0;
 }
