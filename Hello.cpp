7fdd76db726dcd026c0395833c9907c698af9cd9
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-03-30 18:03:07 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,76 +1,54 @@
 #include<iostream>
-#include<string>
 using namespace std;
-class Ship{
-    protected:
-    string name;
-    int fuelCapacity;
-    double valuation;
-    string owningCompany;
-    double insurancePremium;
-
+class Shape{
     public:
-    Ship(string n, int f, double v, string o){
-        name = n;
-        fuelCapacity = f;
-        valuation = v;
-        owningCompany = o;
-        
-    }
-};
-class CruiseShip: public Ship{
-    public:
-    int pools;
-    int passengers;
-    double foodStorage;
-
-    CruiseShip(string n, int f, double v, string o, int c1, int c2, double c3): Ship(n,f,v,o){
-        passengers = c1;
-        pools = c2;
-        foodStorage = c3;
-    }
-
-    void calulcateInsurancePremium(){
-        insurancePremium = (double)(valuation + fuelCapacity + foodStorage)/ (passengers*pools);
-    }
-    void displayDetails(){
-        cout<<"Ship name: "<<name<<endl;
-        cout<<"Ship type: Cruise Ship"<<endl;
-        cout<<"Insurance Premium: "<<insurancePremium<<endl;
-    }
-};
-class CargoCarrier: public Ship{
-    public:
-    int crew;
-    int riskyZones;
-    double inflammables;
-
-    CargoCarrier(string n, int f, double v, string o, int d1, int d2, double d3): Ship(n,f,v,o){
-        crew = d1;
-        riskyZones = d2;
-        inflammables = d3;
-    }
-    void calulcateInsurancePremium(){
-        insurancePremium = (double)((valuation + fuelCapacity)* riskyZones + inflammables)/ (crew*1000);
-    }
-    void displayDetails(){
-        cout<<"Ship name: "<<name<<endl;
-        cout<<"Ship type: Cargo Carrier"<<endl;
-        cout<<"Insurance Premium: "<<insurancePremium<<endl;
-    }
-
+    virtual void area(){}
 };
 
-int main() {
-    CruiseShip cs("Ocean Star", 5000, 2000000, "BlueWave", 200, 4, 1500.5);
-    cs.calulcateInsurancePremium();
-    cs.displayDetails();
+class Circle: public Shape{
+    public:
+    int radius;
+    Circle(int r){
+        radius = r;
+    }
+    void area(){
+        cout<<"Area is: "<<3.14*radius*radius<<endl;
+    }
+};
+class Rectangle: public Shape{
+    public:
+    int length;
+    int breadth;
+    Rectangle(int l, int b){
+        length = l;
+        breadth = b;
+    }
+    void area(){
+        cout<<"Area is: "<<length*breadth<<endl;
+    }
+};
+class Triangle: public Shape{
+    public:
+    int height;
+    int base;
+    Triangle(int h, int b){
+        height = h;
+        base = b;
+    }
+    void area(){
+        cout<<"Area is: "<<0.5*height*base<<endl;
+    }
+};
+int main(){
+    Shape *s;
+    Circle c(10);
+    s = &c;
+    s->area();
+    Rectangle r(5,10);
+    s = &r;
+    s->area();
+    Triangle t(6,2);
+    s = &t;
+    s->area();
 
-    cout<<endl;
-
-   CargoCarrier cc("Cargo King", 8000, 3000000, "SeaTrans", 50, 3, 2500.75);
-    cc.calulcateInsurancePremium();
-    cc.displayDetails();
-
-    return 0;
 }
