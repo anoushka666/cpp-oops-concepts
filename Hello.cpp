473cba902e2867fdb73e73a1d889d59c1f2e5767
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-03-31 08:20:07 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,31 +1,34 @@
 #include<iostream>
 using namespace std;
-class Person{
-    public:
-    Person(int x){
-        cout << "Person::Person(x) called!" << endl;
-    }
-};
-class Faculty : virtual public Person{
-    public:
-    Faculty(int x) : Person(x){
-        cout << "Faculty::Faculty(x) called!" << endl;
-    }
-};
-class Student : virtual public Person{
-    public:
-    Student(int x) : Person(x){
-        cout << "Student::Student(x) called!" << endl;
-    }
-};
-
-class TA : public Faculty, public Student{
-    public:
-    TA(int x) : Person(x), Faculty(x), Student(x){
-        cout << "TA::TA called!" << endl;
-    }
-};
-
 int main(){
-    TA t1(30);
+    double numerator, denominator, arr[4] = {0.0, 0.0, 0.0, 0.0};
+    int index;
+
+    cout<<"Enter array index: ";
+    cin>>index;
+
+    try{
+        //throw exception if array is out of bounds
+        if (index<0 || index>4){
+            throw "Invalid index! Try again";
+        }
+
+        cout<<"Enter numerator: ";
+        cin>>numerator;
+        cout<<"Enter denominator: ";
+        cin>>denominator;
+
+        //throw exception if denominator is 0 
+        if (denominator==0){
+            throw "Invalid denominator! Try again";
+        }
+        else{
+        arr[index] = numerator/denominator;
+        cout<<arr[index]<<endl;
+        }
+    }
+    
+    catch (const char *msg){
+        cout<<msg<<endl;
+    }
 }
