b7550ba561e44ca4c5a89bc2c20cab995ba557f3
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-03-31 07:49:28 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,54 +1,31 @@
 #include<iostream>
 using namespace std;
-class Shape{
+class Person{
     public:
-    virtual void area(){}
+    Person(int x){
+        cout << "Person::Person(x) called!" << endl;
+    }
+};
+class Faculty : virtual public Person{
+    public:
+    Faculty(int x) : Person(x){
+        cout << "Faculty::Faculty(x) called!" << endl;
+    }
+};
+class Student : virtual public Person{
+    public:
+    Student(int x) : Person(x){
+        cout << "Student::Student(x) called!" << endl;
+    }
 };
 
-class Circle: public Shape{
+class TA : public Faculty, public Student{
     public:
-    int radius;
-    Circle(int r){
-        radius = r;
-    }
-    void area(){
-        cout<<"Area is: "<<3.14*radius*radius<<endl;
+    TA(int x) : Person(x), Faculty(x), Student(x){
+        cout << "TA::TA called!" << endl;
     }
 };
-class Rectangle: public Shape{
-    public:
-    int length;
-    int breadth;
-    Rectangle(int l, int b){
-        length = l;
-        breadth = b;
-    }
-    void area(){
-        cout<<"Area is: "<<length*breadth<<endl;
-    }
-};
-class Triangle: public Shape{
-    public:
-    int height;
-    int base;
-    Triangle(int h, int b){
-        height = h;
-        base = b;
-    }
-    void area(){
-        cout<<"Area is: "<<0.5*height*base<<endl;
-    }
-};
+
 int main(){
-    Shape *s;
-    Circle c(10);
-    s = &c;
-    s->area();
-    Rectangle r(5,10);
-    s = &r;
-    s->area();
-    Triangle t(6,2);
-    s = &t;
-    s->area();
-
+    TA t1(30);
 }
