9a339b45261b89cfd5c880fbe8053b139f208d76
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-16 18:42:34 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,40 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-template <class T>
-void returnMaxPair(T a, T b, T c){
-    T sum;
-    if ((a+b)> (b+c) && (a+b)>(c+a)){
-        sum = a+b;
-    }
-    else if ((b+c)> (a+b) && (b+c)>(a+c)){
-        sum = b+c;
-    }
-    else {
-        sum = a+c;
-    }
-    cout<<"Highest sum: "<<sum<<endl;
-}
-template <class T>
-void findMinMax(T arr[], int size, T &minVal, T &maxVal){
-    minVal = arr[0];
-    maxVal = arr[0];
 
-    for (int i=1; i<size; i++){
-        if (arr[i]<minVal){
-            minVal = arr[i];
-        }
-        if (arr[i]>maxVal){
-            maxVal = arr[i];
-        }
-    }
-    cout<<"Minimum value = "<<minVal<<endl;
-    cout<<"Maximum value = "<<maxVal<<endl;
-}
-int main(){
-    returnMaxPair(2,3,1);
-    returnMaxPair(1.5, 3.5, 2.5);
-    int arr1[] = {4,7,1,9,3};
-    int minInt, maxInt;
-    findMinMax(arr1, 5, minInt, maxInt);
+template <class T>
+class Circle {
+    T rad;
 
+public:
+    // Constructor
+    Circle(T r = 0) {
+        rad = r;
+    }
+
+    Circle operator+(T x) {
+        return Circle(rad + x);
+    }
+
+   
+    Circle operator+(Circle c) {
+        return Circle(rad + c.rad);
+    }
+
+    friend ostream& operator<<(ostream &out, const Circle<T> &c) {
+        out << c.rad << " ";
+        return out;
+    }
+};
+
+int main() {
+    Circle<int> c1(5), c2;   
+    c2 = c2 + 3;             
+
+    Circle<int> c3 = c1 + c2;  
+
+    Circle<double> c4(1.5);
+    c4 = c4 + 1.2;             
+
+    cout << c3 << c4 << '\n';
+
+    return 0;
 }
