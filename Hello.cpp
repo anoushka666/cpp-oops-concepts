be6b31de44e575457afb873f5b336db5c83bd867
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-16 18:04:53 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,36 +1,20 @@
+//template to swap 
 #include<iostream>
-#include<string.h>
-#include<cctype>
 using namespace std;
+template <class T>
+void swapValues(T &a, T &b){
+    T temp;
+    temp = a;    
+    a = b;
+    b = temp;
+
+}
 int main(){
-
-    string guess;
-    string words[3] = {"gold", "water", "pillow"};
-    string hints[3] = {"metal used for jewellery", "essential liquid", "gives comfort to head while sleeping"};
-    for (int i=0; i<3; i++){
-        int attempts = 6;
-        cout<<"Hint: "<<hints[i]<<endl;
-        for (int j=0; j<words[i].length(); j++){
-            cout<<"_ ";
-        }
-        cout<<endl;
-       
-
-        while (attempts>0){
-            cout<<"Guess the word!"<<endl;
-            cin>>guess;
-            if (guess==words[i]){
-                cout<<"Correct!"<<endl;
-                break;
-            }
-            else {
-                attempts--;
-                cout<<"Incorrect! Attempts left: "<<attempts<<endl;
-            }
-        }
-        if (attempts==0){
-            cout<<"Game over! The correct word was: "<<words[i]<<endl;
-        }
-        }
-    }
-
+    int i = 4;
+    int j = 5;
+    cout<<"Before swapping, i = "<<i<<" j = "<<j<<endl;
+    swapValues(i,j);
+    cout<<"After swapping, i = "<<i<<" j = "<<j<<endl;
+    
+    
+}
