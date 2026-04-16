61d1e22812c0e918fcc430d9ed8ec3fbab0a9e73
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-16 18:47:59 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,41 +1,30 @@
-#include <iostream>
-using namespace std;
-
-template <class T>
-class Circle {
-    T rad;
-
+#include<iostream>
+class A{
 public:
-    // Constructor
-    Circle(T r = 0) {
-        rad = r;
-    }
-
-    Circle operator+(T x) {
-        return Circle(rad + x);
-    }
-
-   
-    Circle operator+(Circle c) {
-        return Circle(rad + c.rad);
-    }
-
-    friend ostream& operator<<(ostream &out, const Circle<T> &c) {
-        out << c.rad << " ";
-        return out;
+    void p() const{
+        std::cout << "p\n";
     }
 };
 
-int main() {
-    Circle<int> c1(5), c2;   
-    c2 = c2 + 3;             
+template <typename T>
+class B{
+private:
+    T x;
+public:
+    int k;
+    B(T y) : x(y), k(2){
+        x->p();
+    }
+};
 
-    Circle<int> c3 = c1 + c2;  
+template <typename T>
+void f(B<T>& r){
+    std::cout << r.k;
+}
 
-    Circle<double> c4(1.5);
-    c4 = c4 + 1.2;             
-
-    cout << c3 << c4 << '\n';
-
+int main(){
+    A a;
+    B<A*> b(&a);
+    f(b);
     return 0;
 }
