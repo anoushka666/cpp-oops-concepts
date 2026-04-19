2d3067a6354ebcfd66e36af105d4d520cba83cc2
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-19 23:05:06 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,30 +1,54 @@
+//to add elements of various data types 
 #include<iostream>
-class A{
-public:
-    void p() const{
-        std::cout << "p\n";
+using namespace std;
+template <class T>
+class AddElements{
+    public:
+    T element;
+ 
+    AddElements(T a){
+        element = a;
+    }
+    T add( T b){
+        return element + b;
     }
 };
+template <>
 
-template <typename T>
-class B{
-private:
-    T x;
-public:
-    int k;
-    B(T y) : x(y), k(2){
-        x->p();
+class AddElements<string>{
+    public:
+    string element;
+    AddElements(string a){
+        element = a;
+    }
+    string concatenate(string b){
+        return element + b;
     }
 };
-
-template <typename T>
-void f(B<T>& r){
-    std::cout << r.k;
-}
-
 int main(){
-    A a;
-    B<A*> b(&a);
-    f(b);
-    return 0;
+int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
 }
