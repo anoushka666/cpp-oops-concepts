fbda9d194eebec0de79095850ccb2d38a4f7eeb1
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-21 19:56:54 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -6,21 +6,35 @@ struct Node{
     Node* next;
 };
 int main(){
-    //create nodes 
+    //create nodes
     Node* head = new Node{2,nullptr};
-    Node* second = new Node{3,nullptr};
+    Node* second = new Node{3, nullptr};
     Node* third = new Node{4, nullptr};
     Node* fourth = new Node{5, nullptr};
 
-    //link nodes
+    //link the nodes
     head->next = second;
     second->next = third;
     third->next = fourth;
-    
-    //print linked list 
-    Node *temp = head;
-    while (temp!=nullptr){
+
+    //print the linked list 
+    Node* temp = head;
+    while (temp!= nullptr){
         cout<<temp->data<<" ";
         temp = temp->next;
+    }
+
+    cout<<endl<<"After inserting 0 at the beginning"<<endl;
+
+    //insert at the beginning 
+    Node* n = new Node{1};
+    n->next = head;
+    head = n;
+
+    //print the new linked list 
+    Node* temp1 = head;
+    while (temp1!= nullptr){
+        cout<<temp1->data<<" ";
+        temp1 = temp1->next;
     }
 }
