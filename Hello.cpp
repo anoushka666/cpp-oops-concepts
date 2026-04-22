5639f5b5fed0ea9d8d9b4efa9c7fae25ade42359
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-22 16:22:02 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,39 +1,27 @@
 #include<iostream>
 using namespace std;
-class Student{
+template <class T>
+class Calculator{
     public:
-    string name;
-    Student(string n){
-        name = n;
-    }
-    virtual void show(){
-        cout<<"Student Name: "<<name<<endl;
-    }
-};
-class Teacher{
-    public:
-    string name;
-    Teacher(string n){
-        name = n;
-    }
-    virtual void show(){
-        cout<<"Teacher Name: "<<name<<endl;
-    }
-    
-};
-class TeachingAssistant: public Student, public Teacher{
-    public:
-    TeachingAssistant(string s, string t): Student(s), Teacher(t){}
-        void show() override{
-        cout<<"Student Name: "<<Student::name<<endl;
-        cout<<"Teacher Name: "<<Teacher::name<<endl;
-    }
+    T a;
+    T b;
+    Calculator(){
+        cout<<"Enter two numbers: ";
+        cin>>a>>b;
 
+        cout<<"Addition: "<<a+b<<endl;
+        cout<<"Subtraction: "<<a-b<<endl;
+        cout<<"Multiplication: "<<a*b<<endl;
+        if (b==0){
+            cout<<"Division by 0 not possible";
+        }
+        else {
+            cout<<"Division: "<<a/b<<endl;
+        }
+
+    }
 };
 int main(){
-    TeachingAssistant t1("anoushka", "anoushka");
-    t1.show();
-    t1.Student::show();
-    t1.Teacher::show();
-    
+    Calculator <int>c1;
+    Calculator <float>c2;
 }
