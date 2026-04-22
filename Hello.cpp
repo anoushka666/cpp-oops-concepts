46c2b3fc46ff582b9b549c024740d11c1cbcf586
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-22 16:30:33 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,27 +1,65 @@
 #include<iostream>
 using namespace std;
 template <class T>
-class Calculator{
+class MyVector{
+    private:
+    T* arr;        //dynamic array 
+    int capacity;  //total capacity
+    int current;   //current size
+
     public:
-    T a;
-    T b;
-    Calculator(){
-        cout<<"Enter two numbers: ";
-        cin>>a>>b;
+    MyVector(int size = 2){   //constructor
+        capacity = size;
+        current = 0;
+        arr = new T[capacity];
 
-        cout<<"Addition: "<<a+b<<endl;
-        cout<<"Subtraction: "<<a-b<<endl;
-        cout<<"Multiplication: "<<a*b<<endl;
-        if (b==0){
-            cout<<"Division by 0 not possible";
-        }
-        else {
-            cout<<"Division: "<<a/b<<endl;
-        }
+    }
+    ~MyVector(){               //destructor
+        delete[] arr;
+    }
+    //functions
+    int size(){
+        return capacity;
+    }
 
+    void push_back( T data){
+        if (current ==capacity){
+            capacity = capacity*2;
+            T* newarr = new T[capacity];
+
+            for (int i=0; i<capacity; i++){
+                newarr[i] = arr[i];
+
+            }
+            delete[] arr;
+            arr = newarr;
+            
+        }
+        arr[current] = data;
+        current++;
+    }
+
+    void pop_back(){
+        if (current==0){
+            cout<<"Vector is empty!"<<endl;
+        }
+        current--;
+    }
+    void display(){
+        for (int i=0; i<current; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
     }
 };
 int main(){
-    Calculator <int>c1;
-    Calculator <float>c2;
+    MyVector <int> v;
+    v.push_back(20);
+    v.push_back(10);
+    v.push_back(30);
+    v.display();
+
+    v.pop_back();
+    v.display();
+    cout<<"Size: "<<v.size()<<endl;
 }
