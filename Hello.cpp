577587a6210ecf1a72981e1cfd55dcd296473b56
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-24 22:32:25 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,28 +1,51 @@
-//to find second smallest number in an arry
 #include<iostream>
-#include<climits>
 using namespace std;
+struct Node{
+    int data;
+    Node* next;
+};
+void insertAtBeginning(Node* &head, int value){
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+}
+void insertAtEnd(Node* &head, int value){
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = NULL;
+    if(head==NULL){
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void deleteAtBeginning(Node *head){
+    if (head==NULL){
+        return;
+    }
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+}
+void display(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ->";
+        temp = temp->next;
+    }
+    cout<<"NULL";
+}
 int main(){
-    int n;
-    cout<<"Enter size of array: ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter elements of array: ";
-    for (int i=0; i<n; i++){
-        cin>>arr[i];
-    }
-    int smallest = INT_MAX;
-    int secondsmallest = INT_MAX;
+    Node *head = NULL;
+    insertAtBeginning(head, 5);
+    insertAtBeginning(head, 10);
+    insertAtBeginning(head, 15);
+    insertAtEnd(head, 20);
 
-    for (int i=0; i<n; i++){
-        if (arr[i]<smallest){
-            secondsmallest = smallest;
-            smallest = arr[i];
-        }
-        else if(arr[i]>smallest && arr[i]<secondsmallest){
-            secondsmallest = arr[i];
-        }
-    }
-    cout<<"Second smallest element: "<<secondsmallest;
-
+    display(head);
 }
