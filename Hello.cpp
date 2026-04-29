231a65690007a499860ac077d31e92bdb742a330
Anoushka Chandravanshi <anoushka6807@gmail.com>
2026-04-29 19:14:46 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,101 +1,72 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class MyVector {
-    int* arr;
-    int size;
-    int capacity;
-
+class TransportUnit{
 public:
-  
-    MyVector() {
-        capacity = 2;
-        size = 0;
-        arr = new int[capacity];
-    }
+    int load;
+    int distance;
+    int efficiency;
 
-    void resize() {
-        capacity *= 2;
-        int* newArr = new int[capacity];
-        for (int i = 0; i < size; i++)
-            newArr[i] = arr[i];
-        delete[] arr;
-        arr = newArr;
-    }
-
-    void Push(int data) {
-        if (size == capacity)
-            resize();
-        arr[size++] = data;
-    }
-
-    void Push(int data, int index) {
-        if (index < 0 || index > size) {
-            cout << "Invalid index\n";
-            return;
-        }
-
-        if (size == capacity)
-            resize();
-
-        for (int i = size; i > index; i--)
-            arr[i] = arr[i - 1];
-
-        arr[index] = data;
-        size++;
-    }
-
-    int Get(int index) {
-        if (index < 0 || index >= size) {
-            cout << "Invalid index\n";
-            return -1;
-        }
-        return arr[index];
-    }
-
-    void Pop() {
-        if (size == 0) {
-            cout << "Vector empty\n";
-            return;
-        }
-        size--;
-    }
-
-    int Size() {
-        return size;
-    }
-
-    int GetCapacity() {
-        return capacity;
-    }
-
-    void Print() {
-        for (int i = 0; i < size; i++)
-            cout << arr[i] << " ";
-        cout << endl;
-    }
-
-    ~MyVector() {
-        delete[] arr;
+    void inputTransport(){
+        cout<<"Enter load: ";
+        cin>>load;
+        cout<<"Enter distance: ";
+        cin>>distance;
+        efficiency = load / distance;
     }
 };
 
-int main() {
-    MyVector v;
+class NetworkRelay{
+public:
+    int bandwidth;
+    int signal;
 
-    v.Push(10);
-    v.Push(20);
-    v.Push(30);
-    v.Push(15, 1); 
+    void inputRelay(int d){
+        cout<<"Enter bandwidth: ";
+        cin>>bandwidth;
+        signal = bandwidth / d;
+    }
+};
 
-    v.Print(); 
+class AutonomousRig : public TransportUnit, public NetworkRelay{
+public:
+    void inputRig(){
+        cout<<"\n--- Enter details for Autonomous Rig ---\n";
+        inputTransport();              
+        inputRelay(distance);          
+    }
 
-    cout << "Element at 2: " << v.Get(2) << endl;
-    cout << "Size: " << v.Size() << endl;
-    cout << "Capacity: " << v.GetCapacity() << endl;
+    void display(){
+        cout<<"Efficiency: "<<efficiency<<endl;
+        cout<<"Signal: "<<signal<<endl;
+    }
+};
 
-    v.Pop();
-    v.Print();
+
+bool operator>(const AutonomousRig &a1, const AutonomousRig &a2){
+    return (a1.efficiency + a1.signal) > (a2.efficiency + a2.signal);
+}
+
+int main(){
+    cout<<"Evaluating fleet:\n";
+
+    AutonomousRig a1, a2;
+
+    cout<<"\nRig A:";
+    a1.inputRig();
+
+    cout<<"\nRig B:";
+    a2.inputRig();
+
+    cout<<"\n--- Results ---\n";
+    a1.display();
+    a2.display();
+
+    if(a1 > a2){
+        cout<<"Rig A is the superior asset\n";
+    } else {
+        cout<<"Rig B is the superior asset\n";
+    }
 
     return 0;
 }
